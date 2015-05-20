1a9c7946184f58e57473300039801c49bf59b76b
Jansen Vitor <jansen.vitor@sebraemg.com.br>
2015-05-20 18:24:38 -0300

    Update

--- src/resources/views/venda/ver.blade.php
+++ src/resources/views/venda/ver.blade.php
@@ -21,7 +21,7 @@
         <hr >
 
         <div class="row">
-            <div class="col-md-5">
+            <div class="col-md-6">
 
                 <div class="list-group">
                     <a href="#" class="list-group-item disabled">
@@ -31,21 +31,81 @@
                     <a href="#" class="list-group-item">{{ $venda->cliente->pessoa->getDocumento() }}</a>
                 </div>
 
+            </div>
+
+            <div class="col-md-6">
+
+                <div class="list-group">
+                    <a href="#" class="list-group-item disabled">
+                        Faturamento
+                    </a>
+                    <a href="#" class="list-group-item">
+                        <h4>R$ 0,00</h4>
+                    </a>
+                </div>
 
             </div>
 
-            <div class="col-md-7">
+        </div>
+
+        <div class="row">
+
+            <div class="col-md-12">
 
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         Items
+                        <a class="btn btn-xs btn-success pull-right">
+                            <i class="glyphicon glyphicon-plus"></i> Adicionar
+                        </a>
                     </div>
                     <div class="panel-body">
+
+                        <div class="table-responsive">
+                            <table class="table table-condensed table-striped">
+                                <thead>
+                                <tr>
+                                    <th>Descrição</th>
+                                    <th>Qtd.</th>
+                                    <th>Un.</th>
+                                    <th>Valor</th>
+                                    <th>Acrésc.</th>
+                                    <th>Desc.</th>
+                                    <th>Total</th>
+                                    <th width="30">&nbsp;</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @forelse($venda->itens() as $item)
+                                    <tr>
+                                        <td>{{ $item->descricao }}</td>
+                                        <td>{{ $item->quantidade }}</td>
+                                        <td>{{ $item->valor_unitario }}</td>
+                                        <td>{{ $item->valor_bruto }}</td>
+                                        <td>{{ $item->valor_acrescimo }}</td>
+                                        <td>{{ $item->valor_desconto }}</td>
+                                        <td>{{ $item->valor_liquido }}</td>
+                                        <td>
+                                            <a href="#" class="btn btn-danger btn-xs">
+                                                <i class="glyphicon glyphicon-remove"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="8"><i>Nenhum item adicionado</i></td>
+                                    </tr>
+                                @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                 </div>
 
             </div>
 
         </div>
+
     </div>
 @endsection
