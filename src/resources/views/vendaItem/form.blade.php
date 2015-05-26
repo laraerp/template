aad751fa7181d1b8634748f7313355200f40d16b
Jansen Vitor <jansen.vitor@sebraemg.com.br>
2015-05-26 18:22:49 -0300

    Update VendaADDItem

--- src/resources/views/vendaItem/form.blade.php
+++ src/resources/views/vendaItem/form.blade.php
@@ -6,7 +6,7 @@
 
         <div class="row">
             <div class="col-md-6">
-                <h4>Qual(is) produto(s) deseja adicionar?</h4>
+                <h4>Selecione os produtos que deseja adicionar:</h4>
             </div>
             <div class="col-md-6">
                 <div class="pull-right">
@@ -18,7 +18,7 @@
             </div>
         </div>
 
-        <hr >
+        <hr />
 
         <div class="row">
             <div class="col-md-12">
@@ -46,8 +46,12 @@
                             <th><input type="checkbox" id="selectAll" /></th>
                             <th><a href="{{Order::url('codigo')}}">Código</a></th>
                             <th><a href="{{Order::url('nome')}}">Nome</a></th>
-                            <th><a href="{{Order::url('valor')}}">Valor</a></th>
-                            <th width="65">&nbsp;</th>
+                            <th width="90">Quantidade</th>
+                            <th width="90">Unidade</th>
+                            <th width="90">Valor Un.</th>
+                            <th width="90">Acréscimos</th>
+                            <th width="90">Descontos</th>
+                            <th>Total</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -56,15 +60,24 @@
                                 <th scope="row"><input type="checkbox" name="produtos[]" value="{{ $produto->id }}" /></th>
                                 <td>{{ Utils::highlighting($produto->codigo, Input::get('like')) }}</td>
                                 <td>{{ Utils::highlighting($produto->nome, Input::get('like')) }}</td>
-                                <td>{{ $produto->getValor() }}</td>
+                                <td><input type="text" class="form-control input-sm" value="1" /></td>
                                 <td>
-                                    <a href="{{ route('cliente.ver', $produto->id) }}" class="btn btn-info btn-xs">
-                                        <i class="glyphicon glyphicon-eye-open"></i>
-                                    </a>
-                                    <a href="{{ route('cliente.deletar', $produto->id) }}" class="btn btn-danger btn-xs">
-                                        <i class="glyphicon glyphicon-remove"></i>
-                                    </a>
+                                    <select class="form-control input-sm">
+                                        @foreach($unidades as $unidade)
+                                            <optgroup label="{{ $unidade->nome }}">
+
+                                                @foreach($unidade->unidadeMedidas as $medida)
+                                                    <option {{ ($medida->id == $produto->unidade_medida_id) ? 'selected' : '' }}>{{ $medida->simbolo }}</option>
+                                                @endforeach
+
+                                            </optgroup>
+                                        @endforeach
+                                    </select>
                                 </td>
+                                <td><input type="text" class="form-control input-sm" value="{{ $produto->getValorUnitario() }}" /></td>
+                                <td><input type="text" class="form-control input-sm" /></td>
+                                <td><input type="text" class="form-control input-sm" /></td>
+                                <td>{{ $produto->getValorUnitario() }}</td>
                             </tr>
                         @endforeach
                         </tbody>
